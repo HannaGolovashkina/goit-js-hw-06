caf06387e036a8aff5f40a3eada84323e8a2cdda
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2022-10-26 17:57:47 +0300

    task 1and 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ul = document.querySelector("#ingredients");
+const li = ingredients.length;
+for (let index = 0; index < ingredients.length; index++) {
+  const liIngredients = document.createElement("li");
+  liIngredients.textContent = ingredients[index];
+  liIngredients.class = "item";
+  ul.prepend(liIngredients);
+  console.log(ul);
+  console.log(liIngredients.class);
+}
