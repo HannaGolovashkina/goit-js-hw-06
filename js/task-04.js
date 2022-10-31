fad1d765202e157b649de10bb97ddcd8ac4aed45
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2022-10-31 18:20:08 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,19 @@
-// const counterValue = 0;
+let counterValue = 0;
+const buttonDecrement = document.querySelector('[data-action="decrement"]');
+const buttonIncrement = document.querySelector('[data-action="increment"]');
+const value = document.getElementById("value");
 
-// const button = document.querySelector("button");
+currentValue();
 
-// button.addEventListener("click", () => {
-//   console.log("Button was clicked");
-// });
+buttonDecrement.addEventListener("click", () => {
+  counterValue--;
+  currentValue();
+});
+buttonIncrement.addEventListener("click", () => {
+  counterValue++;
+  currentValue();
+});
+
+function currentValue() {
+  value.innerText = counterValue;
+}
