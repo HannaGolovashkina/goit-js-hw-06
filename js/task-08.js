4a0266648da0a34065f382bd5d15f6178b07e5f9
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2022-11-02 10:32:16 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const data = Object.fromEntries(formData.entries());
+  data.email = data.email.trim().toLowerCase();
+  for (const [key, value] of Object.entries(data)) {
+    if (value !== "") continue;
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+    const userDetails = { email: email.value, password: password.value };
+    // elements[key].focus();
+    alert("Все поля должны быть заполнены!");
+    return;
+  }
+  console.log(data);
+}
