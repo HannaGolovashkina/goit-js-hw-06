385e80ba89bada52a3d043d96d6f5129049fac4a
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2022-11-03 11:01:38 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  span: document.querySelector(".color"),
+  button: document.querySelector(".change-color"),
+};
+
+const setBg = (event) => {
+  const randomColor = getRandomHexColor();
+  refs.body.attributes["style"].textContent = randomColor;
+  // refs.body.style.backgroundColor = randomColor;
+  refs.span.innerHTML = randomColor;
+  // refs.span.textContent = randomColor;
+};
+
+refs.button.addEventListener("click", setBg);
+setBg();
